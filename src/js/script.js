cc1a0266dc6c2e2c0568927d24a97ff024319e46
RLOPES43 <RLOPES43@ford.com>
2022-10-31 13:01:10 -0300

    index js

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+import { responseApi } from "./fetchApiResponse.js";
+
+document.addEventListener("DOMContentLoaded", async function () {
+  // Código que será executado quando o navegador carregar
+  await responseApi();
+});
